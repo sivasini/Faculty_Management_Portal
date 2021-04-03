06a5614e6c7568f583002ba5c90017e8d85d3350
Sivasini Netra <45381005+sivasini@users.noreply.github.com>
2021-04-03 14:09:46 +0530

    Delete Sanjith (do this) directory

--- Sanjith (do this)/dash/db.php
+++
@@ -1,6 +0,0 @@
-  
-<?php
-$con = new mysqli("localhost","root","","faculty_dashboard");
-// Check connection
-if(mysqli_connect_errno($con))	echo "Failed to connect MySQL: " .mysqli_connect_error();
-?>
