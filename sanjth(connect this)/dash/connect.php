4dc1bf607f2dae08cd7e47c8932f9ee969c913bc
SanjithRagulV <sanjith.sukratelecom@gmail.com>
2021-04-29 21:26:53 +0530

    Delete sanjth(connect this) directory

--- sanjth(connect this)/dash/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$link = mysqli_connect("localhost", "root", "") or die("Could not connect to server!");
-mysqli_select_db($link, "faculty_dashboard") or die("Could not connect to database!");
-
-?>
