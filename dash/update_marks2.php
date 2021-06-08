06d9b3855a5e964c73eee34ad93273a14d0908f0
Sivasini Netra <45381005+sivasini@users.noreply.github.com>
2021-06-09 00:36:34 +0530

    Validate marks

--- dash/update_marks2.php
+++ dash/update_marks2.php
@@ -12,28 +12,28 @@ $type=$_POST['type'];
 $student=$_POST['studentid'];
 $mark=$_POST['mark'];
 
-if($type=='Assignment#1'){
+if($type=='Assignment#1' and $mark<=10){
   $sql = "update $Course_ID set Assignment1=$mark where Stud_ID='$student' ";
 }
-elseif($type=='Assignment#2'){
+elseif($type=='Assignment#2' and $mark<=10){
   $sql = "update $Course_ID set Assignment2=$mark where Stud_ID='$student' ";
 }
-elseif($type=='Assignment#3'){
+elseif($type=='Assignment#3' and $mark<=10){
   $sql = "update $Course_ID set Assignment3=$mark where Stud_ID='$student' ";
 }
-elseif($type=='Quiz#1'){
+elseif($type=='Quiz#1' and $mark<=10){
   $sql = "update $Course_ID set quiz1=$mark where Stud_ID='$student' ";
 }
-elseif($type=='Quiz#2'){
+elseif($type=='Quiz#2' and $mark<=10){
   $sql = "update $Course_ID set quiz1=$mark where Stud_ID='$student' ";
 }
-elseif($type=='Quiz#3'){
+elseif($type=='Quiz#3' and $mark<=10){
   $sql = "update $Course_ID set quiz3=$mark where Stud_ID='$student' ";
 }
-elseif($type=='Periodicals#1'){
+elseif($type=='Periodicals#1' and $mark<=50){
   $sql = "update $Course_ID set p1=$mark where Stud_ID='$student' ";
 }
-elseif($type=='Periodicals#2'){
+elseif($type=='Periodicals#2' and $mark<=50){
   $sql = "update $Course_ID set p1=$mark where Stud_ID='$student' ";
 }
 
